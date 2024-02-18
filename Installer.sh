4774d769761ec32ee411767dedecd54e5ddae9f3
Fernu <fernu292@gmail.com>
2024-02-18 05:42:39 -0600

    Starting Installer.sh config

---
+++ Installer.sh
@@ -0,0 +1,21 @@
+#
+# Instalador para configurar los dotfiles de 
+# BSPWM y gestionar todas las aplicaciones necesarias
+#
+#
+
+function presentation ()
+{
+	echo " ================================= "
+	echo " Instalador de dotfiles FernuDev   "
+	echo " ================================= "
+
+}
+
+presentation
+
+# Updating system
+
+echo $(sudo pacman -Syu)
+
+
